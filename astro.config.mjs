4ea0637adcb0365f0554390582a45a98148b1fde
Kuttercraft <kuttercraft@gmail.com>
2025-06-04 10:48:14 -0300

    Agrego configuración base del entorno: Astro, TypeScript y Git

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config'
 import node from '@astrojs/node'
 import clerk from '@clerk/astro'
+import dark from '@clerk/themes';
 
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [
+      clerk({
+        appearance: {
+          baseTheme: dark, // Forzar modo oscuro globalmente
+        },
+      }),
+    ],
   adapter: node({ mode: 'standalone' }),
   output: 'server',
 })
